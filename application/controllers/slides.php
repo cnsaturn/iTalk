ad57aa19ef6c5db08b5b04abf2244230e1c91266
HU Yang Gang <yangg.hu@gmail.com>
2012-11-15 04:26:15 +0800

    创建讲稿框架

--- application/controllers/slides.php
+++ application/controllers/slides.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Slides extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -19,7 +19,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('slides');
 	}
 }
 
